8170f34ffeef153700d2c3c492ad108c09323ccb
Khushi Jain <khushijain14908@gmail.com>
2025-07-25 09:59:32 -0700

    Initial Commit

---
+++ BirthdayTracker/BirthdayTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayTrackerApp.swift
+//  BirthdayTracker
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
